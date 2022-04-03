094db6f6eac6c75f728ca21d3a177f40e953655c
Alexandre Boyer <alexandre@holoon.app>
2022-04-03 19:46:49 +0200

    ICanBeUndefined.IsDefined property

--- src/Holoon.NewtonsoftUtils.CanBeUndefined/ICanBeUndefined.cs
+++ src/Holoon.NewtonsoftUtils.CanBeUndefined/ICanBeUndefined.cs
@@ -3,6 +3,10 @@
     internal interface ICanBeUndefined
     {
         bool IsUndefined { get; }
+
+        //NOTE: Added because myproperty.IsUndefined == false is difficult to understand (double negative)
+        public bool IsDefined => !IsUndefined;
+
         object GetValueOrDefault();
     }
 }
